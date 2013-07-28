6b87d492dae46fcd0830557e0a1df44ed750eff4
Michael Perrone <mjperrone@wpi.edu>
2013-07-28 01:44:48 -0400

    setup script now won't rewrite the same thing a bunch of times

--- setup.sh
+++ setup.sh
@@ -1,2 +1,7 @@
-#! /bin/bash
-echo >> ~/.bash_profile "if [ -f ~/dot_files/.bashrc ]; then source ~/dot_files/.bashrc; fi" 
+#! /bin/bash -x
+export STR_IN_PROF=`grep "dot_files" ~/.bash_profile | grep "source" | grep "\.bashrc" | grep "then"`
+if [ -z "$STR_IN_PROF" ]; then
+    echo >> ~/.bash_profile "if [ -f ~/dot_files/.bashrc ]; then source ~/dot_files/.bashrc; fi"
+else
+    echo '' >> /dev/null
+fi
