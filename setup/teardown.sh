e9622a2d6333203fc1864083eb7eb144833e2bbb
Michael Perrone <mjperrone@wpi.edu>
2013-08-21 01:52:29 -0400

    setup and teardown now deal with .vim

--- setup/teardown.sh
+++ setup/teardown.sh
@@ -4,11 +4,16 @@ if [ ! -d $OLD_DOT_FILES ]; then
     exit 0
 fi
 
-
 for f in .bashrc .bash_profile .inputrc .vimrc
 do
     if [ -f $OLD_DOT_FILES/$f ]; then
-        cp $OLD_DOT_FILES/$f ~/$f
+        mv $OLD_DOT_FILES/$f ~/$f
     fi
 done
+if [ -d $OLD_DOT_FILES/.vim ]; then
+    mv -r $OLD_DOT_FILES/.vim ~/
+else
+    rm -rf ~/.vim
+fi
+
 rm -rf $OLD_DOT_FILES
