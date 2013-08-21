c8d66e8e9915fed43d4d8b36e8f18655d2e2cafd
Michael Perrone <3848371+mjperrone@users.noreply.github.com>
2013-08-21 01:52:29 -0400

    setup and teardown now deal with .vim

--- setup/setup.sh
+++ setup/setup.sh
@@ -6,20 +6,25 @@ fi
 
 mkdir $OLD_DOT_FILES
 
-
 for f in .bashrc .bash_profile .inputrc .vimrc
 do
     if [ -f ~/$f ]; then
         mv ~/$f $OLD_DOT_FILES/$f
     fi
 done
+if [ -d ~/$f ]; then
+    mv  ~/.vim $OLD_DOT_FILES/.vim
+fi
 
-cp ~/dot_files/.inputrc ~/.inputrc
-cp ~/dot_files/.vimrc ~/.vimrc
 
-cat << EOF > ~/.bash_profile
-    source ~/.bashrc
-EOF
 cat << EOF > ~/.bashrc
     source ~/dot_files/.bashrc
 EOF
+cat << EOF > ~/.bash_profile
+    source ~/.bashrc
+EOF
+cp ~/dot_files/.inputrc ~/.inputrc
+cp ~/dot_files/.vimrc ~/.vimrc
+cp -r ~/dot_files/.vim ~/
+
+source ~/.bashrc
