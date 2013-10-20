0d49e8cba94ef725b1858d87e7af2fa337d6dde5
Michael Perrone <mjperrone@wpi.edu>
2013-10-19 23:01:48 -0400

    moved bash prompt stuff into its own file, updated the setup, teardown, and .bashrc to source it.

--- setup/setup.sh
+++ setup/setup.sh
@@ -7,7 +7,7 @@ fi
 mkdir $OLD_DOT_FILES
 
 # save old dot files
-for f in .bashrc .bash_profile .inputrc .vimrc .vrapperrc .editrc
+for f in .bashrc .bash_profile .inputrc .vimrc .vrapperrc .editrc .bash_prompt
 do
     if [ -f ~/$f ]; then
         mv ~/$f $OLD_DOT_FILES/$f
@@ -31,6 +31,8 @@ ln -s ~/dot_files/.vimrc ~/.vimrc
 ln -s ~/dot_files/.vrapperrc ~/.vrapperrc
 ln -s ~/dot_files/.editrc ~/.editrc
 ln -s ~/dot_files/.vim ~/.vim
+ln -s ~/dot_files/.bash_prompt ~/.bash_prompt
 
-#apply the changes! (this is why you have to run source ~/dot_files/setup/setup.sh and not sh ~/dot_files/setup/setup.sh
+#apply the changes to the current shell instance!
+#(this is why you have to run source ~/dot_files/setup/setup.sh and not sh ~/dot_files/setup/setup.sh
 source ~/.bashrc
--- setup/teardown.sh
+++ setup/teardown.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 export OLD_DOT_FILES=~/old_dot_files
 if [ ! -d $OLD_DOT_FILES ]; then
-    exit 0 #if there isn't any old stuff, so don't go back
+    exit 0 #if there isn't any old stuff, then don't go back
 fi
 
 #move the old stuff back, overwriting whatever
-for f in .bashrc .bash_profile .inputrc .vimrc .vrapperrc .editrc
+for f in .bashrc .bash_profile .inputrc .vimrc .vrapperrc .editrc .bash_prompt
 do
     if [ -f $OLD_DOT_FILES/$f ]; then
         mv $OLD_DOT_FILES/$f ~/$f
