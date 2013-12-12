ba2b8bbe1bbb82cbdbf1c00e097c1efe5beec52b
Michael Perrone <mjperrone@wpi.edu>
2013-12-12 15:42:18 -0500

    modified teardown from like a month ago, don't know what it does, trust myself i guess.... =/

--- setup/teardown.sh
+++ setup/teardown.sh
@@ -1,22 +1,25 @@
 #!/usr/bin/env bash
 export OLD_DOT_FILES=~/old_dot_files
 if [ ! -d $OLD_DOT_FILES ]; then
-    exit 0 #if there isn't any old stuff, then don't go back
-fi
+    echo "There isn't any old_dot_files folder, so we're going to just leave things be."
+else
+    #move the old stuff back, overwriting whatever
+    for f in .bashrc .bash_profile .inputrc .vimrc .vrapperrc .editrc .bash_prompt .bash_history
+    do
+        if [ -h ~/$f ] || [ -e ~/$f ] ; then
+            rm ~/$f
+        fi
+        if [ -f $OLD_DOT_FILES/$f ]; then
+            mv $OLD_DOT_FILES/$f ~/$f
+        fi
+    done
+    rm -f ~/.vim
+    rm -f ~/.bash_directory_history
 
-#move the old stuff back, overwriting whatever
-for f in .bashrc .bash_profile .inputrc .vimrc .vrapperrc .editrc .bash_prompt .bash_history
-do
-    if [ -f $OLD_DOT_FILES/$f ]; then
-        mv $OLD_DOT_FILES/$f ~/$f
+    if [ -d $OLD_DOT_FILES/.vim ]; then
+        mv $OLD_DOT_FILES/.vim ~/.vim
     fi
-done
-rm -f ~/.vim
-rm -f ~/.bash_directory_history
 
-if [ -d $OLD_DOT_FILES/.vim ]; then
-    mv $OLD_DOT_FILES/.vim ~/.vim
+    #delete what's left
+    rm -rf $OLD_DOT_FILES
 fi
-
-#delete what's left
-rm -rf $OLD_DOT_FILES
