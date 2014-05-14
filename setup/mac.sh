7d5eac4de1b5f2eb239592e488bc70522b3b4e97
Mike Perrone <3848371+mjperrone@users.noreply.github.com>
2014-05-14 17:42:04 -0400

    small changes

--- setup/mac.sh
+++ setup/mac.sh
@@ -1,7 +1,5 @@
 defaults write ~/Library/Preferenc es/.GlobalPreferences com.apple.swipescrolldirection -bool true
-defaults write com.apple.finder AppleShowAllFiles TRUE #show hidden files in
-finder
+defaults write com.apple.finder AppleShowAllFiles TRUE #show hidden files in finder
 easy_install pip
-ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/installe"
+ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
 brew install autojump # https://github.com/joelthelion/autojump
-
