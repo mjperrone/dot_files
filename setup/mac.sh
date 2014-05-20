773d9e036cf7faccba159756c1e5db4416c20f48
Mike Perrone <mike.j.perrone@gmail.com>
2014-05-20 10:48:41 -0700

    I like deafault scrolling now

--- setup/mac.sh
+++ setup/mac.sh
@@ -1,4 +1,3 @@
-defaults write ~/Library/Preferenc es/.GlobalPreferences com.apple.swipescrolldirection -bool true
 defaults write com.apple.finder AppleShowAllFiles TRUE #show hidden files in finder
 easy_install pip
 ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
