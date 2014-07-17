a704be3547f2a736f4704cd7e092d0047638f86f
Mike Perrone <mike.j.perrone@gmail.com>
2014-07-17 00:29:28 -0700

    add zshrc to setup/teardwn

--- setup/setup.sh
+++ setup/setup.sh
@@ -8,7 +8,7 @@ else
     mkdir $OLD_DOT_FILES
 
     # save old dot files
-    for f in .bashrc .bash_profile .bash_prompt .bash_colors .inputrc .editrc .vimrc .vrapperrc .bash_history .gitconfig .gitignore_global
+    for f in .bashrc .bash_profile .bash_prompt .bash_colors .inputrc .editrc .vimrc .vrapperrc .bash_history .gitconfig .gitignore_global .zshrc
     do
         if [ -e ~/$f ]; then
             mv ~/$f $OLD_DOT_FILES/$f
@@ -19,7 +19,7 @@ else
     fi
 
     #link everything to the relevant file in $DOT_FILES?
-    for f in .bashrc .bash_prompt .bash_colors .inputrc .editrc .vimrc .vrapperrc .gitconfig .gitignore_global .vim
+    for f in .bashrc .bash_prompt .bash_colors .inputrc .editrc .vimrc .vrapperrc .gitconfig .gitignore_global .vim .zshrc
     do
         ln -s $DOT_FILES/$f ~/$f
     done
