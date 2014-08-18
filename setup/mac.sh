d44c65a4c68b44ca925ab559fc743eee30f3f2b8
Mike Perrone <3848371+mjperrone@users.noreply.github.com>
2014-08-18 07:07:22 -0700

    remove autojump

--- setup/mac.sh
+++ setup/mac.sh
@@ -1,5 +1,4 @@
 defaults write com.apple.finder AppleShowAllFiles TRUE #show hidden files in finder
 easy_install pip
 ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
-brew install autojump # https://github.com/joelthelion/autojump
 sudo nvram SystemAudioVolume=" " # shhhhhhhhhh on startup.
