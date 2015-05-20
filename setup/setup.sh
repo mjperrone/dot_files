15e48afd897cf9c1c996becc15297bee20863e46
Mike Perrone <3848371+mjperrone@users.noreply.github.com>
2015-05-20 10:17:31 -0700

    setup stuff that I wrote weeks ago and can't remember the purpose of

--- setup/setup.sh
+++ setup/setup.sh
@@ -24,9 +24,18 @@ else
         ln -s $DOT_FILES/$f ~/$f
     done
     ln -s $DOT_FILES/.bashrc ~/.bash_profile #make .bash_profile+.bashrc the same
-    # the difference is documented in , but I want both to do the same thing
+    # the difference is documented at http://www.joshstaiger.org/archives/2005/07/bash_profile_vs.html, but I want both to do the same thing
+    ln -s $DOT_FILE/.vim ~/.nvim
+    ln -s $DOT_FILE/.vimrc ~/.nvimrc
 
     #apply the changes to the current shell instance!
     #(this is why you ought to run source $DOT_FILES/setup/setup.sh and not just sh $DOT_FILES/setup/setup.sh)
     source ~/.bashrc
+
+
+    # make the development folder and checkout some stuff
+    mkdir ~/Development/
+    git clone https://github.com/altercation/solarized.git ~/Development/solarized
+
+    vim -c ":call InstallVundle() | q | q"
 fi
