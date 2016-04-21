680c7fe7a145bd886facd18472e734907c8dbc12
Mike Perrone <mike.j.perrone@gmail.com>
2016-04-21 08:50:32 -0700

    don't use itsalltext anymore

--- .itsalltext.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-osascript -e 'tell application "System Events"
-    tell application "Terminal" to activate
-    end tell'
-osascript -e "tell application \"Terminal\" to do script \"/usr/bin/Vim \\\"$@\\\"\""
