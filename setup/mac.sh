b387d1e38dfc3c542c0ce05572a5253ab8064f1a
Mike Perrone <3848371+mjperrone@users.noreply.github.com>
2016-11-05 13:16:34 -0700

    STUFFFFF

--- setup/mac.sh
+++ setup/mac.sh
@@ -25,11 +25,8 @@ tell application "System Events" to set the autohide of the dock preferences to 
 # un-hide bottom dock bar instantly; no animation
 defaults write com.apple.dock autohide-time-modifier -int 0
 
-# un-hide bottom dock bar after 4 seconds of hovering over the bottom; I rarely
-# use it and it takes up screen real estate
-defaults write com.apple.Dock autohide-delay -float 0
-
-# restart dock to propogate the changes:
+# show the dock after hovering for 2 whole seconds cuz fuck the dock.
+defaults write com.apple.Dock autohide-delay -float 2
 killall Dock
 
 # don't make the cursor big when you shake the mouse a bit
@@ -38,11 +35,9 @@ defaults write ~/Library/Preferences/.GlobalPreferences CGDisableCursorLocationM
 # shhhhhhhhhh on startup.
 sudo nvram SystemAudioVolume=" "
 
-# show the dock after hovering for 2 whole seconds cuz fuck the dock.
-defaults write com.apple.Dock autohide-delay -float 2 && killall Dock
 
 sudo easy_install pip
-ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+# ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew tap neovim/homebrew-neovim
 brew install --HEAD neovim
 brew install ag
@@ -52,7 +47,4 @@ brew install tmux
 brew cask install clipmenu # clipboard history Kreygasm
 brew cask install iterm2
 
-cp $DOT_FILES/setup/iterm_profiles.json "$HOME/Library/Application Support/iTerm2/DynamicProfiles/iterm_profiles.json
-
-echo "run `open Development/solarized/iterm2-colors-solarized/Solarized\ Dark.itermcolors` then Preferences>Profile>Colors>Load Presets..."
-open location "http://iterm2.com/downloads.html"
+cp $DOT_FILES/setup/iterm_profiles.json "$HOME/Library/Application Support/iTerm2/DynamicProfiles/iterm_profiles.json"
