6314cbf6effa480a812c7d3b11737f242f34c933
Mike Perrone <3848371+mjperrone@users.noreply.github.com>
2021-10-10 18:14:08 -0700

    big cleanup: remove bash, clarify setup, document stuff, remove unused stuff

--- setup/setup.sh
+++ setup/setup.sh
@@ -1,45 +1,19 @@
-#!/usr/bin/env bash
+#!/bin/zsh
 
-
-# link everything to the relevant file in $DOT_FILES
-# this is the interface with the programs expecting config files
-for f in .shellrc .inputrc .editrc .vimrc .gitconfig .gitignore_global .vim .zshrc .bashrc .tmux.conf .ignore .psqlrc
+# Programs are expecting this config to be in the home directory. We want to store the source
+# of truth in this repo directory, not the home directory, so we link everything up so they
+# can be found there while living here.
+for f in .editrc .gitconfig .gitignore_global .inputrc .psqlrc .tmux.conf .zshrc 
 do
     ln -s -i $DOT_FILES/$f ~/$f
 done
+
+# Config that needs to be other than home:
 mkdir -p ~/.hammerspoon/
 ln -s -i $DOT_FILES/init.lua ~/.hammerspoon/init.lua
-ln -s -i $DOT_FILES/config.cson ~/.atom/config.cson
-ln -s -i $DOT_FILES/keymap.cson ~/.atom/keymap.cson
 ln -s -i $DOT_FILES/keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
-
-
-ln -s -i $DOT_FILES/.bashrc ~/.bash_profile #make .bash_profile+.bashrc the same
-# the difference is documented at http://www.joshstaiger.org/archives/2005/07/bash_profile_vs.html, but I want both to do the same thing
-mkdir -p ~/.ipython/profile_default/
 
 #apply the changes to the current shell instance!
 #(this is why you ought to run source $DOT_FILES/setup/setup.sh and not just sh $DOT_FILES/setup/setup.sh)
-if [ ! -n $BASH_VERSION ]; then
-    source $DOT_FILES/.bashrc
-elif [ ! -n $ZSH_VERSION ]; then
-    source $DOT_FILES/.zshrc
-fi
-
-
-# make the development folder and checkout some stuff
-mkdir -p ~/code/external/altercation/
-if [ ! -d ~/code/external/altercation/solarized ]; then
-    git clone https://github.com/altercation/solarized.git ~/code/external/altercation/solarized
-fi
-
-mkdir -p ~/code/external/nathanbuchar/
-if [ ! -d ~/code/external/nathanbuchar/atom-one-dark-terminal ]; then
-    git clone git@github.com:nathanbuchar/atom-one-dark-terminal.git ~/code/external/nathanbuchar/atom-one-dark-terminal
-fi
-
-# make zsh default shell
-chsh -s /bin/zsh
-
-mkdir -p ~/.talon/user/community
-ln -i -s ~/code/external/knausj85/knausj_talon ~/.talon/user/knausj_talon
+source $DOT_FILES/.zshrc
+echo 'source $DOT_FILES/.zshrc'
