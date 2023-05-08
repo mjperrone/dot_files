530f4bbc15afbb57486f9c5d8219dd3fd79509aa
Mike Perrone <mike.j.perrone@gmail.com>
2023-05-07 17:13:02 -0700

    set up ansible to install brew packages, remove that from the setup scripts

--- setup/mac_apps.sh
+++ setup/mac_apps.sh
@@ -1,19 +1,3 @@
 #!/bin/zsh
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-brew install gh
-brew install hammerspoon
-brew install starship
-
-brew install --cask caffeine
-brew install --cask spotify
-brew install --cask firefox
-brew install --cask chrome
-brew install --cask discord
-brew install --cask signal
-brew install --cask android-file-transfer
-brew install --cask vlc
-brew install --cask audacity
-brew install --cask visual-studio-code
-
-echo "Open hammerspoon, set it to launch at login, give it accessibility permissions"
